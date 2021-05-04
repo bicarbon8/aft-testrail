c3c4821217d5e01550c9b203cfe0adacbfa9bd1f
Jason Holt Smith <bicarbon8@gmail.com>
2021-05-04 17:26:33 +0100

    repo moved to https://github.com/bicarbon8/automated-functional-testing

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-/** configuration */
-export * from './configuration/testrail-config';
-/** test-cases */
-export * from './test-cases/testrail-test-case-plugin';
-/** logging */
-export * from './logging/testrail-logging-plugin';
